8eee839cc2588d377de09be45ccf22f289df68e7
cesar.ramirez <cesar.ramirez@Tecnologia3.solucionescorp.local>
2017-05-03 12:19:02 -0500

    Pasar a formulario de seleccion de archivos

--- src/pastesci/PasteSCI.java
+++ src/pastesci/PasteSCI.java
@@ -1,82 +1,82 @@
 package pastesci;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 import static java.util.Collections.list;
 import javax.swing.filechooser.FileFilter;
 import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileNameExtensionFilter;
 /**
  * @author cesar.ramirez
  */
-public class PasteSCI {    
+public class PasteSCI {        
+     //** Busqueda de archivos .txt en el directorio para pegar 
+    public static void main(String[] args) throws IOException {
     
-    public static void main(String[] args) {
-        
-     //** Busqueda de archivos .txt en el directorio para pegar     
-        String path; 
+        String path = "", path2 = "" ;
         String files ;
         String f;
         int conteo; 
-        
-
-            //Creamos selector de apertura
-            JFileChooser chooser = new JFileChooser();
-            chooser.setCurrentDirectory(new java.io.File("*.txt"));
-            //Titulo que llevara la ventana
-            chooser.setDialogTitle("Seleccionar carpeta de pegado");
-            //Elegiremos archivos del directorio
-            chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-            chooser.setAcceptAllFileFilterUsed(false);
-            //Si seleccionamos algún archivo retornaremos su directorio
-            if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
-            chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-            //Si no seleccionamos nada retornaremos No seleccion
-       /// ---> mirar!!      System.out.println(chooser.);
-            } else {
-            System.out.println("No seleccion ");
-            }
-        
-        
-        path = "C:\\Users\\cesar.ramirez\\Documents\\para pegar" ;    
-    //**************************
+        JFileChooser chooser = new JFileChooser();
+        chooser.setMultiSelectionEnabled(false);
+        chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        int returnVal = chooser.showOpenDialog(chooser);
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            File file = chooser.getSelectedFile();
+            path = (file.getPath());
+        }
+    //*******************************************************************************************************
         String filesBrowse;
         File folder = new File(path);
-        File[] listOfFiles = folder.listFiles(); 
+        File[] listOfFiles = folder.listFiles();         
         File archivo = null;
         FileReader fr = null;
         BufferedReader br = null;
-        String pegado = "";                             
+        String pegado = ""; 
+        FileWriter flwriter = null;
+        String texto = JOptionPane.showInputDialog("Nombre del archivo a guardar")+".txt"; 
             try {
-               // Apertura del fichero y creacion de BufferedReader para poder
-               // hacer una lectura comoda (disponer del metodo readLine()).           
-            int conteoLinea = 0;                      
-            for (int j = 0; j < listOfFiles.length; j++){
-                if (listOfFiles[j].isFile()){
-                    archivo = new File ("C:\\Users\\cesar.ramirez\\Documents\\para pegar\\"+ 
-                                        listOfFiles[j].getName() );
+               //crea el flujo para escribir en el archivo       
+               flwriter = new FileWriter( path+ "\\"+texto);
+               //crea un buffer o flujo intermedio antes de escribir directamente en el archivo
+               BufferedWriter bfwriter = new BufferedWriter(flwriter);
+               int conteoLinea = 0;                      
+               for (int j = 0; j < listOfFiles.length; j++){
+                    if (listOfFiles[j].isFile()){
+                        archivo = new File (  path + "\\" + listOfFiles[j].getName() );
+                        }
+                    
+                        fr = new FileReader (archivo);
+                        br = new BufferedReader(fr);                      
+                        String linea ;                     
+                            while((linea=br.readLine())!=null) {  
+                                String [] lineaComa = linea.split(",");                          
+                                if (lineaComa.length > 5 ){
+                                conteoLinea++;    
+                                lineaComa[0] = Integer.toString(conteoLinea);
+                                StringBuffer cadena = new StringBuffer();                          
+                                    for (int x=0;x<lineaComa.length;x++){
+                                          cadena =cadena.append(lineaComa[x]+","); 
+                                         pegado = cadena.toString() ;                          } 
+                                         System.out.println(pegado);
+                                         bfwriter.write(pegado);
+                                         bfwriter.newLine();
+                                         
+                                    }          
+                                }       
+                            }
+               
+                bfwriter.close();   
+                JOptionPane.showMessageDialog(null,"Archivo "+ texto + " creado satisfactoriamente..");
                 }
-                    fr = new FileReader (archivo);
-                    br = new BufferedReader(fr);
-                    String linea ;                     
-                    while((linea=br.readLine())!=null) {  
-                        String [] lineaComa = linea.split(",");                          
-                        if (lineaComa.length > 5 ){
-                        conteoLinea++;    
-                        lineaComa[0] = Integer.toString(conteoLinea);
-                        StringBuffer cadena = new StringBuffer(); 
-                        for (int x=0;x<lineaComa.length;x++){
-                             cadena =cadena.append(lineaComa[x]+","); 
-                             pegado = cadena.toString() ;                          } 
-                             System.out.println(pegado); 
-                        }                             
-                    }
-            }
-        }
             
         catch(Exception e){
+            
         e.printStackTrace();       
         }
             
@@ -91,10 +91,12 @@ public class PasteSCI {
                }
                
                catch (Exception e2){ 
+                   
                e2.printStackTrace();
                }
-            }           
-   }    
+               
+          }           
+    }    
 }
     
 
